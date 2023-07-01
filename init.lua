75d660389fd6cc11bc464789ca44109fb15210b0
Miguel Montalvo <miguel.montalvo@archeytpethemes.co>
2023-06-30 23:26:00 -0700

    🔥 Remove lualine

--- init.lua
+++ init.lua
@@ -99,20 +99,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
