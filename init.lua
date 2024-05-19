577241228286adbe32a7740aaeaa7fc5a0e5accd
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2024-05-18 21:38:19 -0400

    🔧 Update config

--- init.lua
+++ init.lua
@@ -160,11 +160,6 @@ require('lazy').setup({
 
           -- Lesser used LSP functionality
           nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-          -- Create a command `:Format` local to the LSP buffer
-          vim.api.nvim_buf_create_user_command(event.buf, 'Format', function(_)
-            vim.lsp.buf.format()
-          end, { desc = 'Format current buffer with LSP' })
         end,
       })
 
@@ -217,6 +212,44 @@ require('lazy').setup({
       -- After setting up mason-lspconfig you may set up servers via lspconfig
       require('lspconfig').shopify_theme_ls.setup {}
     end,
+  },
+
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    lazy = false,
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        javascript = { { 'prettier' } },
+        liquid = { { 'prettier' } },
+      },
+    },
   },
 
   {
