67e36e62af0a37d688db1ead86d84167fc8f3812
Miguel Montalvo <montalvomiguelo@users.noreply.github.com>
2025-02-28 19:12:09 -0600

    Remove redundant comment

--- init.lua
+++ init.lua
@@ -136,7 +136,6 @@ require('lazy').setup({
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
