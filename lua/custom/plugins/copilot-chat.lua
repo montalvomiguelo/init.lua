640b073ee301e18083af1c8797e9a1278487dd3b
Miguel Montalvo <montalvomiguelo@users.noreply.github.com>
2025-05-11 13:44:28 -0600

    Update copilot-chat.lua

--- lua/custom/plugins/copilot-chat.lua
+++ lua/custom/plugins/copilot-chat.lua
@@ -9,6 +9,61 @@ return {
     opts = {
       -- See Configuration section for options
     },
+    keys = {
+      { '<c-s>', '<CR>', ft = 'copilot-chat', desc = 'Submit Prompt', remap = true },
+      { '<leader>a', '', desc = '+ai', mode = { 'n', 'v' } },
+      {
+        '<leader>aa',
+        function()
+          return require('CopilotChat').toggle()
+        end,
+        desc = 'Toggle (CopilotChat)',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<leader>ax',
+        function()
+          return require('CopilotChat').reset()
+        end,
+        desc = 'Clear (CopilotChat)',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<leader>aq',
+        function()
+          vim.ui.input({
+            prompt = 'Quick Chat: ',
+          }, function(input)
+            if input ~= '' then
+              require('CopilotChat').ask(input)
+            end
+          end)
+        end,
+        desc = 'Quick Chat (CopilotChat)',
+        mode = { 'n', 'v' },
+      },
+      {
+        '<leader>ap',
+        function()
+          require('CopilotChat').select_prompt()
+        end,
+        desc = 'Prompt Actions (CopilotChat)',
+        mode = { 'n', 'v' },
+      },
+    },
+    config = function(_, opts)
+      local chat = require 'CopilotChat'
+
+      vim.api.nvim_create_autocmd('BufEnter', {
+        pattern = 'copilot-chat',
+        callback = function()
+          vim.opt_local.relativenumber = false
+          vim.opt_local.number = false
+        end,
+      })
+
+      chat.setup(opts)
+    end,
     -- See Commands section for default commands if you want to lazy load on them
   },
 }
