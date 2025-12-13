269711da9c178687d4d44acf8dccdf144d583bfd
Miguel Montalvo <montalvomiguelo@users.noreply.github.com>
2025-12-13 15:05:49 -0600

    Refactor VSCode keymaps to use function calls

--- lua/custom/plugins/vscode.lua
+++ lua/custom/plugins/vscode.lua
@@ -2,6 +2,8 @@ if not vim.g.vscode then
   return {}
 end
 
+local vscode = require 'vscode'
+
 -- Add some vscode specific keymaps
 vim.api.nvim_create_autocmd('User', {
   pattern = 'VSCodeOverrides',
@@ -9,22 +11,46 @@ vim.api.nvim_create_autocmd('User', {
   callback = function()
     -- VSCode-specific keymaps for search and navigation
     vim.keymap.set('n', '<leader><space>', '<cmd>Find<cr>')
-    vim.keymap.set('n', '<leader>F', [[<cmd>lua require('vscode').action('workbench.action.findInFiles')<cr>]])
-    vim.keymap.set('n', '<leader>rn', "<Cmd>call VSCodeNotify('editor.action.rename')<CR>")
-    vim.keymap.set('n', '<leader>ca', "<Cmd>call VSCodeNotify('editor.action.quickFix')<CR>")
-    vim.keymap.set('n', 'gr', "<Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>")
-    vim.keymap.set('n', 'gI', "<Cmd>call VSCodeNotify('editor.action.goToImplementation')<CR>")
-    vim.keymap.set('n', '<leader>D', "<Cmd>call VSCodeNotify('editor.action.goToTypeDefinition')<CR>")
-    vim.keymap.set('n', 'gD', "<Cmd>call VSCodeNotify('editor.action.goToDeclaration')<CR>")
+    vim.keymap.set('n', '<leader>F', function()
+      vscode.call 'workbench.action.findInFiles'
+    end)
+    vim.keymap.set('n', '<leader>rn', function()
+      vscode.call 'editor.action.rename'
+    end)
+    vim.keymap.set('n', '<leader>ca', function()
+      vscode.call 'editor.action.quickFix'
+    end)
+    vim.keymap.set('n', 'gr', function()
+      vscode.call 'editor.action.referenceSearch.trigger'
+    end)
+    vim.keymap.set('n', 'gI', function()
+      vscode.call 'editor.action.goToImplementation'
+    end)
+    vim.keymap.set('n', '<leader>D', function()
+      vscode.call 'editor.action.goToTypeDefinition'
+    end)
+    vim.keymap.set('n', 'gD', function()
+      vscode.call 'editor.action.goToDeclaration'
+    end)
 
     -- Diagnostic keymaps
-    vim.keymap.set('n', '[d', "<Cmd>call VSCodeNotify('editor.action.marker.prev')<CR>")
-    vim.keymap.set('n', ']d', "<Cmd>call VSCodeNotify('editor.action.marker.next')<CR>")
-    vim.keymap.set('n', '<leader>e', "<Cmd>call VSCodeNotify('editor.action.showHover')<CR>")
+    vim.keymap.set('n', '[d', function()
+      vscode.call 'editor.action.marker.prev'
+    end)
+    vim.keymap.set('n', ']d', function()
+      vscode.call 'editor.action.marker.next'
+    end)
+    vim.keymap.set('n', '<leader>e', function()
+      vscode.call 'editor.action.showHover'
+    end)
 
     -- Navigate VSCode tabs like lazyvim buffers
-    vim.keymap.set('n', '<S-h>', "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
-    vim.keymap.set('n', '<S-l>', "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
+    vim.keymap.set('n', '<S-h>', function()
+      vscode.call 'workbench.action.previousEditor'
+    end)
+    vim.keymap.set('n', '<S-l>', function()
+      vscode.call 'workbench.action.nextEditor'
+    end)
   end,
 })
 
