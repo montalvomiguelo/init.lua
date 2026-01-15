7e09d6691b65922038b13bdcf8f19818fb3cbe3f
Miguel Montalvo <montalvomiguelo@users.noreply.github.com>
2026-01-15 07:51:29 -0600

    Change key mapping for Ack command to <leader>f

--- init.lua
+++ init.lua
@@ -386,7 +386,7 @@ require('lazy').setup({
   {
     'montalvomiguelo/ack.vim',
     keys = {
-      { '<leader>a', ':Ack!<Space>', mode = 'n', desc = '[S]earch by [G]rep' },
+      { '<leader>f', ':Ack!<Space>', mode = 'n', desc = '[S]earch by [G]rep' },
     },
     init = function()
       vim.g.ackprg = 'rg --vimgrep'
