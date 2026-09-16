9d1c6efdb3ba666ec1512d82b6661f005705b582
Miguel Montalvo <montalvomiguelo@users.noreply.github.com>
2026-09-15 22:13:37 -0600

    Add LazyDev integration for Lua filetype

--- init.lua
+++ init.lua
@@ -321,6 +321,16 @@ require('lazy').setup({
 
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
+        per_filetype = {
+          lua = { inherit_defaults = true, 'lazydev' },
+        },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            score_offset = 100,
+          },
+        },
       },
 
       cmdline = {
